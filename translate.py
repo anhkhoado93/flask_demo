e28f3a8d3bf7b9cd096a542d10096c19d7356eb3
Khoa Do <kdo@Khoas-Air.net.fpt>
2021-10-21 03:17:15 +0700

    add model, translate

--- translate.py
+++ translate.py
@@ -1,18 +1,83 @@
-def read_vocab(path):
-    vocab = dict()
+"""
+!pip install pyvi
+!pip install https://github.com/trungtv/vi_spacy/raw/master/packages/vi_spacy_model-0.2.1/dist/vi_spacy_model-0.2.1.tar.gz
+!gdown https://drive.google.com/uc?id=1kZnu9yI385y78u1IznWwNj5Js6Vep30n
+"""
+
+import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchtext
+from torchtext.legacy.data import Field, BucketIterator
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import spacy
+from torchtext.legacy import data
+from model import Encoder, Decoder, Seq2Seq
+
+def load_vocab(path):
+    stoi_vocab = dict()
+    itos_vocab = dict()
     index = -1
     with open(path, 'r') as f:
         for line in f:
             nline = line[:-1]
             index, token =nline.split('\t',1)
-            vocab[token] = int(index)
-    return vocab
+            stoi_vocab[token] = int(index)
+            itos_vocab[str(index)] = token
+    return [stoi_vocab, itos_vocab]
 
-SRC_VOCAB = read_vocab('resources/vocabs/src.txt')
-TRG_VOCAB = read_vocab('resources/vocabs/trg.txt')
+def load_model(path, src_vocab, trg_vocab): 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    INPUT_DIM = len(SRC.vocab)
+    OUTPUT_DIM = len(TRG.vocab)
+    HID_DIM = 256
+    ENC_LAYERS = 3
+    DEC_LAYERS = 3
+    ENC_HEADS = 8
+    DEC_HEADS = 8
+    ENC_PF_DIM = 512
+    DEC_PF_DIM = 512
+    ENC_DROPOUT = 0.1
+    DEC_DROPOUT = 0.1
 
-def tokenize(sentence):
-    return sentence.split(' ')
+    enc = Encoder(INPUT_DIM, 
+                HID_DIM, 
+                ENC_LAYERS, 
+                ENC_HEADS, 
+                ENC_PF_DIM, 
+                ENC_DROPOUT, 
+                device)
+
+    dec = Decoder(OUTPUT_DIM, 
+                HID_DIM, 
+                DEC_LAYERS, 
+                DEC_HEADS, 
+                DEC_PF_DIM, 
+                DEC_DROPOUT, 
+                device)
+    SRC_PAD_IDX = src_vocab[0]['<pad>']
+    TRG_PAD_IDX = trg_vocab[0]['<pad>']
+
+    model = Seq2Seq(enc, dec, SRC_PAD_IDX, TRG_PAD_IDX, device).to(device)
+    return model
+
+
+
+
+SRC_VOCAB = load_vocab('resources/vocabs/src.txt')
+TRG_VOCAB = load_vocab('resources/vocabs/trg.txt')
+MODEL     = load_model('resources/model/tut6-model.pt')
+
+def stoi(VO): pass
+def itos(VO): pass
+def tokenize_vn(text):
+    """
+    Tokenizes English text from a string into a list of strings (tokens)
+    """
+    return [tok.text for tok in spacy_vn.tokenizer(text)]
 
 def translate(sentence, src_field, trg_field, model, device, max_len = 50):
     
@@ -24,9 +89,9 @@ def translate(sentence, src_field, trg_field, model, device, max_len = 50):
     else:
         tokens = [token.lower() for token in sentence]
 
-    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+    tokens = ['<sos>'] + tokens + ['<eos>']
         
-    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+    src_indexes = [src_field[0][token] for token in tokens]
 
     src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
     
@@ -35,7 +100,7 @@ def translate(sentence, src_field, trg_field, model, device, max_len = 50):
     with torch.no_grad():
         enc_src = model.encoder(src_tensor, src_mask)
 
-    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+    trg_indexes = [trg_field[0]['<sos>']]
 
     for i in range(max_len):
 
@@ -50,15 +115,15 @@ def translate(sentence, src_field, trg_field, model, device, max_len = 50):
         
         trg_indexes.append(pred_token)
 
-        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+        if pred_token == trg_field[0]['<eos>']:
             break
     
-    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+    trg_tokens = [trg_field[1][i] for i in trg_indexes]
     
     return ' '.join(trg_tokens[1:])
 
-    
+
 def correct(sentence):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    lst = tokenize(sentence)
-    return translate(sentence, SRC_VOCAB, SRC_VOCAB, model, device)
+    lst = tokenize_vn(sentence)
+    return translate(sentence, SRC_VOCAB, SRC_VOCAB, MODEL, device)
